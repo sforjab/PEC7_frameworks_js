1c7b7fb88accaab99043933b4a16eb91a612851d
Sergio Forja Barriga <sergio.forja88@gmail.com>
2023-07-01 01:13:32 +0200

    Implementación de componente article-detail y su template

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Article } from '../models/article';
+import { ArticleService } from '../services/article-service.service';
 
 @Component({
   selector: 'app-article-detail',
   templateUrl: './article-detail.component.html',
   styleUrls: ['./article-detail.component.css']
 })
-export class ArticleDetailComponent {
+export class ArticleDetailComponent implements OnInit {
+  articleId!: number;
+  article!: Article;
 
+  constructor(
+    private route: ActivatedRoute,
+    private articleService: ArticleService
+  ) {}
+
+  ngOnInit() {
+    this.articleId = +this.route.snapshot.params['id'];
+    this.getArticleDetails();
+  }
+
+  getArticleDetails() {
+    this.articleService.getArticles().subscribe((articles: Article[]) => {
+      this.article = articles.find(article => article.id === this.articleId) as Article;
+      if (this.article === undefined) {
+        // Manejar si no se encuentra el artículo
+      }
+    });
+  }
 }
